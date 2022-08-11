36bde09d0a01ece5c1398c230283251ac364b615
Maxim Bagryancev <m.bagryancev@mechanica.agency>
2022-08-11 15:38:26 +0400

    added .editorconfig + layout for main pages and added layout empty for login and register pages

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'materialize-css/dist/js/materialize.min'//подключаем стили от materialize
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from "@/views/Home";
 
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
     name: 'home',
+    meta: {layout: 'main'},
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('@/views/Login')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Categories')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Register')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Detail-record')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('@/views/History')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Planning')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Profile')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('@/views/Record')
   },
 ]
 
