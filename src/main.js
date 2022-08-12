40784029122d1dd11d20f0e53112fa5a11ac4562
Maxim Bagryancev <m.bagryancev@mechanica.agency>
2022-08-12 10:27:29 +0400

    added lib vuelidate + validate form register

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
+import {Vuelidate} from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import 'materialize-css/dist/js/materialize.min'//подключаем стили от materialize
 import dateFilter from '@/filters/date.filter'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'//подключаем стили от materialize
+
 
 Vue.config.productionTip = false
 
+Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
 new Vue({
